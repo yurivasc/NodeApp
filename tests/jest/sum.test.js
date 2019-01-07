d8fe5b4958837d8dd1ae97f21bae8c1b35542ec7
yvs <yurifx@hotmail.com>
2019-01-07 15:17:30 +1300

    tests

--- tests/jest/sum.test.js
+++ tests/jest/sum.test.js
@@ -7,5 +7,25 @@ test("sum numbers", () => {
 
 test("sum null", () => {
   var res = sum(1, null);
-  expect(res).toBe(null);
+  expect(res).toBe(1);
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("simple tests", () => {
+  const value = 5;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(6);
+  expect(value).toBeLessThanOrEqual(5);
+});
+
+test("test arrays", () => {
+  const list = ["water", "lettuce", "tomatos"];
+
+  expect(list).toContain("water");
 });
