d9276637a605847652af9ac429f5893aade792d4
yvs <yurifx@hotmail.com>
2019-01-07 17:22:43 +1300

    enable cors

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const port = process.env.PORT  || 3000;
 
 var app = express();
 
+//Enable cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 var obj = {
     name: "yuri",
